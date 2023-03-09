4c6011b5bc69bab45912a41dae06fbb396ba422d
alessiogentilezza <alessio.gentilezza@gmail.com>
2023-03-09 14:42:11 +0100

    sostituzione array con oggetti

---
+++ js/main.js
@@ -0,0 +1,104 @@
+const imgArray = [
+    {
+        image: 'img/01.webp',
+        title: 'Marvel\'s Spiderman Miles Morale',
+        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+    },
+    {
+        image: 'img/02.webp',
+        title: 'Ratchet & Clank: Rift Apart',
+        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+    },
+    {
+        image: 'img/03.webp',
+        title: 'Fortnite',
+        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+    },
+    {
+        image: 'img/04.webp',
+        title: 'Stray',
+        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+    },
+    {
+        image: 'img/05.webp',
+        title: "Marvel's Avengers",
+        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+    }
+];
+
+const containerImg = document.querySelector('.img-container');
+const containerImgThumble = document.querySelector('.img-container-thumble');
+
+let containerImgContent = "";
+let containerImgContentThumble = "";
+
+for (let i = 0; i < imgArray.length; i++) {
+    let key = imgArray[i];
+
+    const imgWrapper = `<div class="wrapper-img">
+        <img src="${key.image}">
+        <p class="title">${key.title}</p>
+        <p class="text">${key.text}</p>
+                            </div>`;
+    const imgWrapperThumble = `<div class="wrapper-img-thumble">
+    <img src="${key.image}">
+                            </div>`;
+    containerImgContent += imgWrapper;
+    containerImgContentThumble += imgWrapperThumble;
+}
+
+containerImg.innerHTML = containerImgContent;
+containerImgThumble.innerHTML = containerImgContentThumble;
+
+const wrapperImg = document.getElementsByClassName('wrapper-img');
+const wrapperImgThumble = document.getElementsByClassName('wrapper-img-thumble');
+
+let activeImage = 0;
+
+wrapperImg[activeImage].classList.add('d-block');
+wrapperImgThumble[activeImage].classList.add('brightness');
+
+
+const nextDom = document.querySelector('#next');
+const prevDom = document.querySelector('#prev');
+
+console.log(activeImage);
+
+nextDom.addEventListener('click',
+    function () {
+
+        console.log(activeImage);
+
+        wrapperImg[activeImage].classList.remove('d-block');
+        wrapperImgThumble[activeImage].classList.remove('brightness');
+
+        if (activeImage == wrapperImg.length - 1) {
+            activeImage = 0;
+        } else {
+            activeImage = activeImage + 1;
+        }
+        console.log(activeImage);
+
+        wrapperImg[activeImage].classList.add('d-block');
+        wrapperImgThumble[activeImage].classList.add('brightness');
+    }
+);
+
+prevDom.addEventListener('click',
+    function () {
+
+        wrapperImg[activeImage].classList.remove('d-block');
+        wrapperImgThumble[activeImage].classList.remove('brightness');
+
+
+        if (activeImage == 0) {
+            activeImage = wrapperImg.length - 1;
+        } else {
+            activeImage = activeImage - 1;
+        }
+
+        wrapperImg[activeImage].classList.add('d-block');
+        wrapperImgThumble[activeImage].classList.add('brightness');
+
+    }
+);
